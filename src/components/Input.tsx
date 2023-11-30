0856fd9e2e9d4eecefcddf87d9917b279995372a
nirgluzman <nir.gluzman@gmail.com>
2023-11-30 13:37:47 +0100

    add custom Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,18 @@
+// custom Input component
+
+type InputProps = {
+	label: string;
+	id: string;
+};
+
+export default function Input({label, id}: InputProps) {
+	return (
+		<p>
+			<label htmlFor={id}>{label}</label>
+			<input
+				id={id}
+				type='text'
+			/>
+		</p>
+	);
+}
