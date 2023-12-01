fefa69166ddcf6fcf11cc98a09994bb1d2b08783
nirgluzman <nir.gluzman@gmail.com>
2023-12-01 08:46:18 +0100

    add comments on drwabacks with Button2 using Type Predicate

--- src/components/Button2.tsx
+++ src/components/Button2.tsx
@@ -1,11 +1,16 @@
 import {type ComponentPropsWithoutRef} from 'react';
 
 // Discriminating Union - enables the creation of a type that can represent several different possibilities or variants.
-type ButtonProps = ComponentPropsWithoutRef<'button'>;
-type AnchorProps = ComponentPropsWithoutRef<'a'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {href?: never};
+type AnchorProps = ComponentPropsWithoutRef<'a'> & {href?: string};
 
-// 'href' property excits only with anchor <a> element, not in <button>.
+// 'href' attribute exists only with anchor <a> element, not in <button>.
 // helper function returns Type Predicate - type assertion that checks if an object has a specific property or set of properties.
+//
+// Drawbacks:
+// 'href' attribute is optional - what happens when 'href' is absence? we cannot infer <a>
+// this means, for example, that <a> can include attributes of <button> when 'href' is absence.
+// BUT, when 'href' exists then TS knows it is <a> and therefore watches the attributes passed with props.
 function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
 	return 'href' in props;
 }
