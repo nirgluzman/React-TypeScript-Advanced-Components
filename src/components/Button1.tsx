f7fdcfa284338a7e9737c735297d21a068f6599f
nirgluzman <nir.gluzman@gmail.com>
2023-12-01 08:14:18 +0100

    rename Button to Button1, in order to introduce another solution

--- src/components/Button1.tsx
+++ src/components/Button1.tsx
@@ -9,7 +9,7 @@ type AnchorProps = {
 	el: 'anchor';
 } & ComponentPropsWithoutRef<'a'>;
 
-export default function Button(props: ButtonProps | AnchorProps) {
+export default function Button1(props: ButtonProps | AnchorProps) {
 	if (props.el === 'anchor') {
 		return (
 			<a
