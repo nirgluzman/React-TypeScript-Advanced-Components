66328b6331b28462b88f3628a9ff7e371885b596
nirgluzman <nir.gluzman@gmail.com>
2023-12-01 18:36:05 +0100

    add Card component which has multiple slots for content

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Input from './components/Input';
 import Button1 from './components/Button1';
 import Button2 from './components/Button2';
 import Container from './components/Container';
+import Card from './components/Card';
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 				label='Your name'
 				type='text'
 			/>
+
 			<Input
 				id='age'
 				label='Your age'
@@ -20,6 +22,7 @@ function App() {
 			<p>
 				<Button1 el='button'>a Button</Button1>
 			</p>
+
 			<p>
 				<Button1
 					el='anchor'
@@ -27,19 +30,29 @@ function App() {
 					A Link
 				</Button1>
 			</p>
+
 			<p>
 				<Button2>a Button</Button2>
 			</p>
+
 			<p>
 				<Button2 href='https://google.com'>A Link</Button2>
 			</p>
+
 			<Container
 				as={Button1}
 				el='button'
 				onClick={() => {}}>
 				Click me!
 			</Container>
+
 			<Container as='button'>Hello!</Container>
+
+			<Card
+				title='My Card'
+				actions={<button onClick={() => console.log('Button clicked!')}>Click Me!</button>}>
+				<p>Some content</p>
+			</Card>
 		</main>
 	);
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,23 @@
+// Card component which has multiple "slots" for content:
+// Main slot -> children prop
+// Actions slot -> actions prop
+
+import {type ReactNode} from 'react';
+
+type CardProps = {
+	title: string;
+	actions: ReactNode;
+	children: ReactNode;
+	// "actions" is like an extra "slot" of this component.
+	// it is the same type as the children prop, since we expect JSX code as a prop value.
+};
+
+export default function Card({title, actions, children}: CardProps) {
+	return (
+		<section>
+			<h2>{title}</h2>
+			{children}
+			{actions}
+		</section>
+	);
+}
